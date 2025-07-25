4750a8100ac5167fd70f19c1b0ca4ce2a73f0ae2
Sreenath Somarajapuram <somarajapuram@gmail.com>
2025-07-24 19:21:51 -0700

    [project] Basic structure - Removing boilerplate

--- src/pyliteral/core.py
+++
@@ -1,2 +0,0 @@
-def hello(name: str = "world") -> str:
-    return f"Hello, {name}!"
--- tests/test_core.py
+++
@@ -1,5 +0,0 @@
-from pyliteral import hello
-
-def test_hello():
-    assert hello("PyPI") == "Hello, PyPI!"
-    assert hello() == "Hello, world!"
